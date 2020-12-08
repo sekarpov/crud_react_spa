b152a904636e971cad6bd9f5be1067e93bf8308e
skarpov <sergo8ck@gmail.com>
2020-12-08 11:53:44 +0300

    Route and Controller methods

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request, Task $task)
+    {
+        $allTasks = $task->whereIn('user_id', $request->user())->with('user');
+        $tasks = $allTasks->orderBy('created_at', 'desc')->take(20)->get();
+
+        return response()->json([
+            'tasks' => $tasks
+        ]);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255'
+        ]);
+
+        $task = $request->user()->task()->create([
+            'name' => $request->name
+        ]);
+
+        return response()->json($task->with('user')->find($task->id));
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
